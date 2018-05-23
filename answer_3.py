3b60240de81981fecdbd6d323efc27bbf56f71bf
harshitahluwalia7895 <harshit.ahluwalia7895889924@gmail.com>
2018-05-23 22:30:31 +0530

    final Commit

---
+++ answer_3.py
@@ -0,0 +1,5 @@
+#Answer 3 -->
+                x = input('Enter First Number: ')
+                y = input('Enter Second Number: ')
+                z = input('Enter Third Number: ')
+                print('The value of x you have entered is:',x)
