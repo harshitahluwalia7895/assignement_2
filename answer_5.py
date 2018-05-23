244a37303e22ebb1a7aa22be7addfb6c2e8860bd
harshitahluwalia7895 <harshit.ahluwalia7895889924@gmail.com>
2018-05-23 22:32:25 +0530

    final Commit

---
+++ answer_5.py
@@ -0,0 +1,5 @@
+#Answer 5 -->
+                s="Acadview"
+                course="python"
+                fees=5000
+                print('The value in s is ' ,s, '\nThe value in course is ',course,'\nThe value in fees is ',fees)
